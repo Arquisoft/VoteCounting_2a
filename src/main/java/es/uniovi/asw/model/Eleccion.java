600e8a5c6f48d5954e86fd3c0caba849d54bf207
edugarcia22 <edugm192@gmail.com>
2016-04-01 18:29:09 +0200

    Añadir comprobación atributos

--- src/main/java/es/uniovi/asw/model/Eleccion.java
+++ src/main/java/es/uniovi/asw/model/Eleccion.java
@@ -12,7 +12,7 @@ public class Eleccion extends AbstractPersistable<Long> {
 
 	private static final long serialVersionUID = 1L;
 
-	//private static final Logger log = LoggerFactory.getLogger(Eleccion.class);
+	private static final Logger log = LoggerFactory.getLogger(Eleccion.class);
 
 	@Column(nullable = false, unique = true)
 	private String nombre;
@@ -29,7 +29,8 @@ public class Eleccion extends AbstractPersistable<Long> {
 	}
 
 	public void setNombre(String nombre) {
-		this.nombre = nombre;
+		if(nombre != null && !nombre.isEmpty())
+		   this.nombre = nombre;
 	}
 
 	public Long getId() {
@@ -37,7 +38,10 @@ public class Eleccion extends AbstractPersistable<Long> {
 	}
 
 	public void setId(Long id) {
-		this.id = id;
+		if(id > 0)
+		  this.id = id;
+		else
+		  log.error("El id debe ser correcto, mayor que 0");
 	}
 
 
