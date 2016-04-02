b9c6e8992b4967b00f154348ab795da401c50411
adrianigl24 <uo209738@uniovi.es>
2016-04-02 11:47:42 +0200

    cambios en el repository

--- src/main/java/es/uniovi/asw/repository/VotoRepository.java
+++ src/main/java/es/uniovi/asw/repository/VotoRepository.java
@@ -1,5 +1,11 @@
 package es.uniovi.asw.repository;
 
-public class VotoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.uniovi.asw.model.Voto;
+
+public interface VotoRepository extends JpaRepository<Voto, Long> {
+	List<Voto> findAll();
 }
