9e256812ff9099d2ab87433ed216295ce682313d
adrianigl24 <uo209738@uniovi.es>
2016-04-06 23:42:06 +0200

    recuentoImpl

--- src/main/java/es/uniovi/asw/service/impl/RecuentoServiceImpl.java
+++ src/main/java/es/uniovi/asw/service/impl/RecuentoServiceImpl.java
@@ -1,8 +1,26 @@
 package es.uniovi.asw.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.uniovi.asw.model.Recuento;
+import es.uniovi.asw.repository.RecuentoRepository;
+import es.uniovi.asw.service.RecuentoService;
 
 @Service
 public class RecuentoServiceImpl implements RecuentoService {
 
-	
+	@Autowired
+	private RecuentoRepository recuentoRepository;
+
+	@Override
+	public List<Recuento> publicarRecuento(Long idEleccion) {
+		if (idEleccion == null) {
+			throw new IllegalArgumentException("idEleccion no puede ser nulo");
+		}
+
+		return this.recuentoRepository.findByIdEleccion(idEleccion);
+	}
 }
