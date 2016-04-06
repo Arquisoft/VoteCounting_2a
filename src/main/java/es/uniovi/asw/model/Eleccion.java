667671e17f1213ad3adf3148063d22df2469bc71
edugarcia22 <edugm192@gmail.com>
2016-04-06 19:48:24 +0200

    Update Eleccion.java

--- src/main/java/es/uniovi/asw/model/Eleccion.java
+++ src/main/java/es/uniovi/asw/model/Eleccion.java
@@ -3,7 +3,8 @@ package es.uniovi.asw.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
@@ -29,7 +30,8 @@ public class Eleccion extends AbstractPersistable<Long> {
 	}
 
 	public void setNombre(String nombre) {
-		this.nombre = nombre;
+		if (nombre != null && !nombre.isEmpty())
+		   this.nombre = nombre;
 	}
 
 	public Long getId() {
@@ -37,10 +39,7 @@ public class Eleccion extends AbstractPersistable<Long> {
 	}
 
 	public void setId(Long id) {
-		this.id = id;
+		if(id > 0)
+		  this.id = id;
 	}
-
-
-	
-	
 }
