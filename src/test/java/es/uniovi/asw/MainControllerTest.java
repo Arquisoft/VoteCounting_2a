35521dbb705afff36af071c1c2e2d963d5b237ba
daniarango94 <daniarango94@gmail.com>
2016-04-08 10:09:54 +0200

    Añadida excepción para manejar valores no válidos

--- src/test/java/es/uniovi/asw/MainControllerTest.java
+++ src/test/java/es/uniovi/asw/MainControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -24,21 +23,20 @@ import org.springframework.web.context.WebApplicationContext;
 @IntegrationTest({ "server.port=0" })
 public class MainControllerTest {
 
-  @Autowired
-  private WebApplicationContext context;
+	@Autowired
+	private WebApplicationContext context;
 
-  private MockMvc mvc;
+	private MockMvc mvc;
 
-  @Before
-  public void setUp() throws Exception {
-    mvc = MockMvcBuilders.webAppContextSetup(context).build();
-  }
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
 
-  @Test
-  public void testLanding() throws Exception {
-    mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string(containsString("Voting")));
-  }
-  
-  
+	@Test
+	public void testLanding() throws Exception {
+		mvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Voting")));
+	}
 
 }
