4de11a7c84c2411b9e69baf8983e1e9b0725e331
adrianigl24 <uo209738@uniovi.es>
2016-04-12 16:36:55 +0200

    cambios bbdd

--- src/ddl/03.datos.sql
+++ src/ddl/03.datos.sql
@@ -1,3 +1,6 @@
 INSERT INTO `counters`.`colegio_electoral`(`id`,`nombre`) VALUES (1, 'Ciudad Naranco');
 INSERT INTO `counters`.`colegio_electoral`(`id`,`nombre`) VALUES (2, 'La Corredoria');
 
+INSERT INTO `counters`.`eleccion`(`id`,`nombre`) VALUES (1, '27N');
+INSERT INTO `counters`.`eleccion`(`id`,`nombre`) VALUES (2, '4A');
+
