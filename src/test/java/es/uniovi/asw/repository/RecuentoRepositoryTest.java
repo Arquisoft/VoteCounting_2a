1c3dd437ddcd7e0a4fe2bb74ab70d34ad3ca297e
adrianigl24 <uo209738@uniovi.es>
2016-04-19 21:07:50 +0200

    Bug error testing

--- src/test/java/es/uniovi/asw/repository/RecuentoRepositoryTest.java
+++ src/test/java/es/uniovi/asw/repository/RecuentoRepositoryTest.java
@@ -4,19 +4,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.awt.print.Pageable;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import es.uniovi.asw.Application;
-import es.uniovi.asw.model.Recuento;
+import junit.framework.TestCase;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -26,64 +23,14 @@ public class RecuentoRepositoryTest {
 
 	@Autowired
 	private RecuentoRepository recuento;
-	private Sort sort;
-	private Iterable<Long> ids;
+
 	private Long id;
-	private Pageable page = null;
 	private Long idEleccion;
 
 	@Test
 	public void testFindAll() {
 
-		assertTrue(recuento.findAll() == null);
-
-	}
-
-	@Test
-	public void testFindAllSort() {
-
-		if (sort == null)
-			assertTrue(recuento.findAll(sort) == null);
-		else
-			assertFalse(recuento.findAll(sort) == null);
-
-	}
-
-	@Test
-	public void testFindAllIds() {
-
-		if (ids == null)
-			assertTrue(recuento.findAll(ids) == null);
-		else
-			assertFalse(recuento.findAll(ids) == null);
-
-	}
-
-	@Test
-	public <S extends Recuento> void testSaveIterable() {
-
-		Iterable<S> entities = null;
-		assertTrue(recuento.save((Iterable<S>) entities) == null);
-
-	}
-
-	@Test
-	public void testGetOne() {
-
-		if (id == null)
-			assertTrue(recuento.getOne(-1L) == null);
-		else
-			assertFalse(recuento.getOne(id) == null);
-
-	}
-
-	@Test
-	public void testFindAllPage() {
-
-		if (page == null)
-			assertTrue(recuento.findAll((Sort) page) == null);
-		else
-			assertFalse(recuento.findAll((Sort) page) == null);
+		TestCase.assertNotNull(recuento.findAll());
 
 	}
 
@@ -118,10 +65,9 @@ public class RecuentoRepositoryTest {
 	public void testFindByIdEleccion() {
 
 		if (idEleccion == null)
-			assertTrue(recuento.findByIdEleccion(idEleccion) == null);
+			TestCase.assertNotNull(recuento.findByIdEleccion(-1L));
 		else
-			assertFalse(recuento.findByIdEleccion(idEleccion) == null);
-
+			TestCase.assertNotNull(recuento.findByIdEleccion(idEleccion));
 	}
 
 }
--- src/test/java/es/uniovi/asw/repository/VotoRepositoryTest.java
+++ src/test/java/es/uniovi/asw/repository/VotoRepositoryTest.java
@@ -4,14 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.awt.print.Pageable;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -27,66 +24,15 @@ public class VotoRepositoryTest {
 
 	@Autowired
 	private VotoRepository voto;
-	private Sort sort;
-	private Iterable<Long> ids;
+
 	private Long id;
-	private Pageable page = null;
+
 	private Long idEleccion;
 
 	@Test
 	public void testFindAll() {
 
-		assertTrue(voto.findAll() == null);
-
-	}
-
-	@Test
-	public void testFindAllSort() {
-
-		if (sort == null)
-			assertTrue(voto.findAll(sort) == null);
-		else
-			assertFalse(voto.findAll(sort) == null);
-
-	}
-
-	@Test
-	public void testFindAllIds() {
-
-		if (ids == null)
-			assertTrue(voto.findAll(ids) == null);
-		else
-			assertFalse(voto.findAll(ids) == null);
-
-	}
-
-	@Test
-	public <S extends Voto> void testSaveIterable() {
-
-		Iterable<S> entities = null;
-		assertTrue(voto.save((Iterable<S>) entities) == null);
-
-	}
-
-
-
-	@Test
-	public void testGetOne() {
-
-		if (id == null)
-			assertTrue(voto.getOne(-1L) == null);
-		else
-			assertFalse(voto.getOne(id) == null);
-
-	}
-
-	@Test
-	public void testFindAllPage() {
-
-		if (page == null)
-			assertTrue(voto.findAll((Sort) page) == null);
-		else
-			assertFalse(voto.findAll((Sort) page) == null);
+		assertTrue(voto.findAll() != null);
 
 	}
 
@@ -94,11 +40,11 @@ public class VotoRepositoryTest {
 	public <S extends Voto> void testSaveS() {
 
 		Voto v = new Voto();
-		v.setIdColegio(new Long (1));
+		v.setIdColegio(new Long(1));
 		v.setIdEleccion(new Long(1));
 		v.setOpcion("No");
 		v = this.voto.save(v);
-		
+
 		TestCase.assertNotNull(v);
 
 	}
@@ -134,7 +80,7 @@ public class VotoRepositoryTest {
 	public void testFindByIdEleccion() {
 
 		if (idEleccion == null)
-			assertTrue(voto.findByIdEleccion(idEleccion) == null);
+			assertFalse(voto.findByIdEleccion(idEleccion) == null);
 		else
 			assertFalse(voto.findByIdEleccion(idEleccion) == null);
 
