8d3cf00e0314d755a333c37503a801a254fd86b9
daniarango94 <daniarango94@gmail.com>
2016-04-21 17:39:16 +0200

    Ampliado testVoto

--- src/test/java/es/uniovi/asw/model/VotoTest.java
+++ src/test/java/es/uniovi/asw/model/VotoTest.java
@@ -6,74 +6,86 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class VotoTest {
-	
-	
+
 	private Voto v = new Voto();
-	
 
 	@Test
 	public void testIdColegio() {
-		
-		v.setIdColegio((long)2);
-		assertTrue(v.getIdColegio().equals((long)2));
+
+		v.setIdColegio((long) 2);
+		assertTrue(v.getIdColegio().equals((long) 2));
 		assertFalse(v.getIdColegio().equals(null));
-		
-		v.setIdColegio((long)0);
-		assertFalse(v.getIdColegio().equals((long)0));
-		
-		v.setIdColegio((long)-10);
-		assertFalse(v.getIdColegio().equals((long)-10));
-		
+
+		v.setIdColegio((long) 0);
+		assertFalse(v.getIdColegio().equals((long) 0));
+		assertTrue(v.getIdColegio().equals((long) 2));
+
+		v.setIdColegio((long) -10);
+		assertFalse(v.getIdColegio().equals((long) -10));
+		assertTrue(v.getIdColegio().equals((long) 2));
+
 		v.setIdColegio(null);
 		assertFalse(v.getIdColegio().equals(null));
-		
+		assertTrue(v.getIdColegio().equals((long) 2));
+
 	}
-	
-	
+
 	@Test
-	public void testIdEleccion(){
-		
-		v.setIdEleccion((long)5);
-		assertTrue(v.getIdEleccion().equals((long)5));
+	public void testIdEleccion() {
+
+		v.setIdEleccion((long) 5);
+		assertTrue(v.getIdEleccion().equals((long) 5));
 		assertFalse(v.getIdEleccion().equals(null));
-		
-		v.setIdEleccion((long)0);
-		assertFalse(v.getIdEleccion().equals((long)0));
-		
-		v.setIdEleccion((long)-20);
-		assertFalse(v.getIdEleccion().equals((long)-20));
-		
+
+		v.setIdEleccion((long) 0);
+		assertTrue(v.getIdEleccion().equals((long) 5));
+		assertFalse(v.getIdEleccion().equals((long) 0));
+		assertFalse(v.getIdEleccion().equals(null));
+
+		v.setIdEleccion((long) -20);
+		assertFalse(v.getIdEleccion().equals((long) -20));
+		assertTrue(v.getIdEleccion().equals((long) 5));
+
 		v.setIdEleccion(null);
+		assertTrue(v.getIdEleccion().equals((long) 5));
+		assertFalse(v.getIdEleccion().equals((long) -20));
 		assertFalse(v.getIdEleccion().equals(null));
-		
+
 	}
-	
-	
+
 	@Test
-	public void testOpcion(){
-		
+	public void testOpcion() {
+
 		v.setOpcion("Si");
 		assertTrue(v.getOpcion().equals("Si"));
-		
+
 		v.setOpcion(null);
+		assertTrue(v.getOpcion().equals("Si"));
 		assertFalse(v.getOpcion().equals(null));
-		
+
 		v.setOpcion("No");
 		assertTrue(v.getOpcion().equals("No"));
 		assertFalse(v.getOpcion().equals("Si"));
 		assertFalse(v.getOpcion().equals(null));
-		
+
 		v.setOpcion("");
+		assertTrue(v.getOpcion().equals("No"));
 		assertFalse(v.getOpcion().equals(""));
-		
+		assertFalse(v.getOpcion().equals("Si"));
+		assertFalse(v.getOpcion().equals(null));
+
 	}
-	
+
 	@Test
-	public void testOnline(){
-		
+	public void testOnline() {
+
 		v.setOnline(false);
-		assertTrue(v.isOnline() == false);
-		assertFalse(v.isOnline() == true);
+		assertTrue(!v.isOnline());
+		assertFalse(v.isOnline());
+
+		v.setOnline(true);
+		assertTrue(v.isOnline());
+		assertFalse(!v.isOnline());
 	}
 
 }
